ab37e73b2f7727a6a99cdcc4e623bdabbc0ede7c
lw <623656827@qq.com>
2019-08-20 17:51:59 +0800

    99

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,12 @@
-const add = require('../main');
+const createMultiplyTable = require('../main');
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+it ('should show chengfabiao', () => {
+    expect(createMultiplyTable(2, 4)).toBe(
+        "2*2=4"
+        +"\n"+"2*3=6 3*3=9"
+        +"\n"+"2*4=8 3*4=12 4*4=16\n"
+    );
+        // '2*2=4\
+        // 2*3=6 3*3=9\
+        // 2*4=8 3*4=12 4*4=16');
 });
--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-function add (left, right) {
-    return left + right;
+function createMultiplyTable (left, right) {
+    if(check(left,right)==""){
+        return "";
+    }
+    var str="";
+    str=concat(left,right);
+    return str;
+}
+function check(left,right){
+    if(left>right){
+        return "";
+    }
+}
+function concat(left,right){
+    let str="";
+    for(let i=0;i<right-left+1;i++){
+        for(let j=0;j<=i;j++){
+            if(j!==0){
+                str+=" ";
+            }
+            str+=getDiviStr(left,i,j);
+            
+        }
+        
+        str+="\n";
+    }
+    return str;
+}
+function getDiviStr(left,i,j){
+    let diviStr="";
+    diviStr+=((left+j)+"*"+(left+i)+"="+((left+j)*(left+i)));
+    return diviStr;
 }
 
-module.exports = add;
+module.exports = createMultiplyTable;
